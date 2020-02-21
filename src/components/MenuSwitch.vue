713ec5a9252e372be5b2b92c77157910c867135a
Iago Goncalves <iago.goncalves@klickpages.com.br>
2020-02-21 12:05:25 -0300

    realizando request para pegar as configuracoes

---
+++ src/components/MenuSwitch.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="menu--switch">
+    <ul class="container no-padding">
+      <li>
+        <i class="icon-kp-kp"></i>
+        Klickpages
+      </li>
+      <li class="myKlicksendMenu">
+        <a :href="klicksendUrl" data-turbolinks="false">
+          <i class="icon-kp-ks"></i>
+          Klicksend
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    klicksendUrl: String,
+  },
+};
+</script>
