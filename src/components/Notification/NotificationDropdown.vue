9aac5f0c9087a5c3239a11c4edf32a6d8806164e
Iago Goncalves <iago.goncalves@klickpages.com.br>
2020-05-12 12:41:57 -0300

    adiciona botao de notificacoes

---
+++ src/components/Notification/NotificationDropdown.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="nav-notifications" style="display:block">
+    <div class="new-notifications-signal"></div>
+    <a href="javascript:;" data-toggle="dropdown" class="dropdown-toggle">
+      <notification-icon />
+    </a>
+    <ul class="drop__list drop__list--notifications dropdown-menu"></ul>
+  </div>
+</template>
+
+<script>
+import NotificationIcon from '../icons/Notification.vue';
+
+export default {
+  components: {
+    NotificationIcon,
+  },
+};
+</script>
