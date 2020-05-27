b5cecb64034d6b94a7bcd9f622776b497c6220de
Iago Goncalves <iago.goncalves@klickpages.com.br>
2020-05-27 14:39:22 -0300

    mudado versao no package.json

--- dist/klickpages-header.umd.js
+++ dist/klickpages-header.umd.js
@@ -18406,6 +18406,38 @@ hash.ripemd160 = hash.ripemd.ripemd160;
 
 /***/ }),
 
+/***/ "7db0":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var $ = __webpack_require__("23e7");
+var $find = __webpack_require__("b727").find;
+var addToUnscopables = __webpack_require__("44d2");
+var arrayMethodUsesToLength = __webpack_require__("ae40");
+
+var FIND = 'find';
+var SKIPS_HOLES = true;
+
+var USES_TO_LENGTH = arrayMethodUsesToLength(FIND);
+
+// Shouldn't skip holes
+if (FIND in []) Array(1)[FIND](function () { SKIPS_HOLES = false; });
+
+// `Array.prototype.find` method
+// https://tc39.github.io/ecma262/#sec-array.prototype.find
+$({ target: 'Array', proto: true, forced: SKIPS_HOLES || !USES_TO_LENGTH }, {
+  find: function find(callbackfn /* , that = undefined */) {
+    return $find(this, callbackfn, arguments.length > 1 ? arguments[1] : undefined);
+  }
+});
+
+// https://tc39.github.io/ecma262/#sec-array.prototype-@@unscopables
+addToUnscopables(FIND);
+
+
+/***/ }),
+
 /***/ "7dd0":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -37406,12 +37438,15 @@ var MailServiceLink_component = normalizeComponent(
 )
 
 /* harmony default export */ var MailServiceLink = (MailServiceLink_component.exports);
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"7ff2bf82-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/notification/NotificationDropdown.vue?vue&type=template&id=cc59582c&
-var NotificationDropdownvue_type_template_id_cc59582c_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"nav-notifications",class:{open: _vm.notificationsOpen},staticStyle:{"display":"block"},on:{"click":function($event){return _vm.toggleNotifications()}}},[_c('div',{staticClass:"new-notifications-signal"}),_c('a',{staticClass:"dropdown-toggle",attrs:{"href":"javascript:;","data-toggle":"dropdown"}},[_c('notification-icon')],1),_c('notification-list',{attrs:{"notificationsOpen":_vm.notificationsOpen}})],1)}
-var NotificationDropdownvue_type_template_id_cc59582c_staticRenderFns = []
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"7ff2bf82-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/notification/NotificationDropdown.vue?vue&type=template&id=2ea61343&
+var NotificationDropdownvue_type_template_id_2ea61343_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"nav-notifications",class:{open: _vm.notificationsOpen},staticStyle:{"display":"block"},on:{"click":function($event){return _vm.toggleNotifications()}}},[_c('div',{staticClass:"new-notifications-signal",class:_vm.newNotifications ? 'active' : ''}),_c('a',{staticClass:"dropdown-toggle",attrs:{"href":"javascript:;","data-toggle":"dropdown"}},[_c('notification-icon')],1),_c('notification-list',{attrs:{"notificationsOpen":_vm.notificationsOpen},on:{"notificationsReaded":_vm.readNotifications}})],1)}
+var NotificationDropdownvue_type_template_id_2ea61343_staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/notification/NotificationDropdown.vue?vue&type=template&id=cc59582c&
+// CONCATENATED MODULE: ./src/components/notification/NotificationDropdown.vue?vue&type=template&id=2ea61343&
+
+// EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.find.js
+var es_array_find = __webpack_require__("7db0");
 
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"7ff2bf82-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/icons/Notification.vue?vue&type=template&id=20ab1395&
 var Notificationvue_type_template_id_20ab1395_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('svg',{attrs:{"xmlns":"http://www.w3.org/2000/svg","viewBox":"0 0 512 512"}},[_c('path',{attrs:{"d":"m450.201 407.453c-1.505-.977-12.832-8.912-24.174-32.917-20.829-44.082-25.201-106.18-25.201-150.511 0-.193-.004-.384-.011-.576-.227-58.589-35.31-109.095-85.514-131.756v-34.657c0-31.45-25.544-57.036-56.942-57.036h-4.719c-31.398 0-56.942 25.586-56.942 57.036v34.655c-50.372 22.734-85.525 73.498-85.525 132.334 0 44.331-4.372 106.428-25.201 150.511-11.341 24.004-22.668 31.939-24.174 32.917-6.342 2.935-9.469 9.715-8.01 16.586 1.473 6.939 7.959 11.723 15.042 11.723h109.947c.614 42.141 35.008 76.238 77.223 76.238s76.609-34.097 77.223-76.238h109.947c7.082 0 13.569-4.784 15.042-11.723 1.457-6.871-1.669-13.652-8.011-16.586zm-223.502-350.417c0-14.881 12.086-26.987 26.942-26.987h4.719c14.856 0 26.942 12.106 26.942 26.987v24.917c-9.468-1.957-19.269-2.987-29.306-2.987-10.034 0-19.832 1.029-29.296 2.984v-24.914zm29.301 424.915c-25.673 0-46.614-20.617-47.223-46.188h94.445c-.608 25.57-21.549 46.188-47.222 46.188zm60.4-76.239c-.003 0-213.385 0-213.385 0 2.595-4.044 5.236-8.623 7.861-13.798 20.104-39.643 30.298-96.129 30.298-167.889 0-63.417 51.509-115.01 114.821-115.01s114.821 51.593 114.821 115.06c0 .185.003.369.01.553.057 71.472 10.25 127.755 30.298 167.286 2.625 5.176 5.267 9.754 7.861 13.798z"}})])}
@@ -37439,12 +37474,12 @@ var Notification_component = normalizeComponent(
 )
 
 /* harmony default export */ var Notification = (Notification_component.exports);
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"7ff2bf82-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/notification/NotificationList.vue?vue&type=template&id=539dacfc&
-var NotificationListvue_type_template_id_539dacfc_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('vue-custom-scrollbar',{staticClass:"drop__list drop__list--notifications dropdown-menu",attrs:{"tagname":'ul'},on:{"ps-y-reach-end":_vm.onInfiniteScroll}},[_vm._l((_vm.notifications.items),function(notification){return _c('notification-item',{key:("" + (notification.id)),attrs:{"notification":notification}})}),(_vm.showNotificationError)?_c('notification-error',{attrs:{"errorMessage":_vm.errorMessage},on:{"refreshNotifications":_vm.refreshNotifications}}):_vm._e(),(_vm.isLoading)?_c('loader'):_vm._e()],2)}
-var NotificationListvue_type_template_id_539dacfc_staticRenderFns = []
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"7ff2bf82-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/notification/NotificationList.vue?vue&type=template&id=5452a82a&
+var NotificationListvue_type_template_id_5452a82a_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('vue-custom-scrollbar',{staticClass:"drop__list drop__list--notifications dropdown-menu",attrs:{"tagname":'ul'},on:{"ps-y-reach-end":_vm.onInfiniteScroll}},[_vm._l((_vm.notifications.items),function(notification){return _c('notification-item',{key:("" + (notification.id)),attrs:{"notification":notification}})}),(_vm.showNotificationError)?_c('notification-error',{attrs:{"errorMessage":_vm.errorMessage},on:{"refreshNotifications":_vm.refreshNotifications}}):_vm._e(),(_vm.isLoading)?_c('loader'):_vm._e()],2)}
+var NotificationListvue_type_template_id_5452a82a_staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/notification/NotificationList.vue?vue&type=template&id=539dacfc&
+// CONCATENATED MODULE: ./src/components/notification/NotificationList.vue?vue&type=template&id=5452a82a&
 
 // EXTERNAL MODULE: ./node_modules/vue-custom-scrollbar/dist/vueScrollbar.umd.min.js
 var vueScrollbar_umd_min = __webpack_require__("4b70");
@@ -39768,6 +39803,9 @@ var ITEMS_PER_PAGE = 5;
                 return _this3.readNotifications(_this3.notifications.items);
 
               case 2:
+                _this3.$emit('notificationsReaded');
+
+              case 3:
               case "end":
                 return _context3.stop();
             }
@@ -39786,7 +39824,7 @@ var ITEMS_PER_PAGE = 5;
             switch (_context4.prev = _context4.next) {
               case 0:
                 if (!(_this4.shouldLoadMoreNotifications && !_this4.isLoading)) {
-                  _context4.next = 4;
+                  _context4.next = 5;
                   break;
                 }
 
@@ -39795,6 +39833,11 @@ var ITEMS_PER_PAGE = 5;
                 return _this4.refreshNotifications();
 
               case 4:
+                if (_this4.notificationsOpen) {
+                  _this4.sendReadNotification();
+                }
+
+              case 5:
               case "end":
                 return _context4.stop();
             }
@@ -39837,8 +39880,8 @@ var ITEMS_PER_PAGE = 5;
 
 var NotificationList_component = normalizeComponent(
   notification_NotificationListvue_type_script_lang_js_,
-  NotificationListvue_type_template_id_539dacfc_render,
-  NotificationListvue_type_template_id_539dacfc_staticRenderFns,
+  NotificationListvue_type_template_id_5452a82a_render,
+  NotificationListvue_type_template_id_5452a82a_staticRenderFns,
   false,
   null,
   null,
@@ -39848,6 +39891,8 @@ var NotificationList_component = normalizeComponent(
 
 /* harmony default export */ var NotificationList = (NotificationList_component.exports);
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/notification/NotificationDropdown.vue?vue&type=script&lang=js&
+
+
 //
 //
 //
@@ -39863,14 +39908,32 @@ var NotificationList_component = normalizeComponent(
 //
 //
 //
+//
+//
+//
+
 
 
 /* harmony default export */ var NotificationDropdownvue_type_script_lang_js_ = ({
   data: function data() {
     return {
-      notificationsOpen: false
+      notificationsOpen: false,
+      notificationsReaded: false
     };
   },
+  computed: _objectSpread2({}, Object(vuex_esm["b" /* mapGetters */])({
+    notifications: 'notification/notifications'
+  }), {
+    newNotifications: function newNotifications() {
+      if (this.notificationsReaded) {
+        return false;
+      }
+
+      return !!this.notifications.items.find(function (notification) {
+        return notification.read === false;
+      });
+    }
+  }),
   components: {
     NotificationIcon: Notification,
     NotificationList: NotificationList
@@ -39883,6 +39946,9 @@ var NotificationList_component = normalizeComponent(
       if (!this.$el.contains(event.target)) {
         this.notificationsOpen = false;
       }
+    },
+    readNotifications: function readNotifications() {
+      this.notificationsReaded = true;
     }
   },
   created: function created() {
@@ -39904,8 +39970,8 @@ var NotificationList_component = normalizeComponent(
 
 var NotificationDropdown_component = normalizeComponent(
   notification_NotificationDropdownvue_type_script_lang_js_,
-  NotificationDropdownvue_type_template_id_cc59582c_render,
-  NotificationDropdownvue_type_template_id_cc59582c_staticRenderFns,
+  NotificationDropdownvue_type_template_id_2ea61343_render,
+  NotificationDropdownvue_type_template_id_2ea61343_staticRenderFns,
   false,
   null,
   null,
