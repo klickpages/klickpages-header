a7c8450db0bb914e4ca707103875ba61be8d8e9d
Yuri <yuri.soares@klickpages.com.br>
2020-06-29 22:29:03 -0300

    KP-155 #comment Adiciona componente com a barra universal hotmart

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <hotmart-header
+      v-if="topBarConfig.user.ucode"
+      :hotmartProURL="hotmartProURL"
+    />
     <klickpages-header
       :klickartURL="klickartURL"
       :jwtSecret="jwtSecret"
@@ -8,7 +12,8 @@
 </template>
 
 <script>
-
+import { mapGetters } from 'vuex';
+import HotmartHeader from '@/components/HotmartHeader.vue';
 import KlickpagesHeader from '@/components/KlickpagesHeader.vue';
 
 export default {
@@ -17,9 +22,14 @@ export default {
     return {
       klickartURL: 'https://art.klickpages.local',
       jwtSecret: '986392d822a953640a12c03aa3dc6799',
+      hotmartProURL: 'https://extensions-scripts.buildstaging.com/app.js',
     };
   },
+  computed: mapGetters({
+    topBarConfig: 'topBar/config',
+  }),
   components: {
+    HotmartHeader,
     KlickpagesHeader,
   },
 };
---
+++ src/components/HotmartHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HotmartHeader',
+  props: {
+    hotmartProURL: String,
+  },
+  mounted() {
+    const hotmartHeader = document.createElement('script');
+    hotmartHeader.src = this.hotmartProURL;
+    document.body.append(hotmartHeader);
+  },
+};
+</script>
